7e2bd6741c3f6ad2da9ce0965b9babae3d00aead
altguerrero <josealberto.guerreroayala@gmail.com>
2024-06-25 13:26:27 -0500

    feat(store-modal) Add StoreDetailsDialog component and update Map component to use it

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as OrderForm } from "./OrderForm";
 export { default as SearchPlaces } from "./SearchPlaces";
 export { default as Store } from "./Store";
 export { default as RoutesRenderers } from "./RoutesRenderers";
+export { default as StoreDetailsDialog } from "./StoreDetailsDialog";
